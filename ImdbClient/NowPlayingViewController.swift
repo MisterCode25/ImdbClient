266809034b3d13173efc71e5b6243c0a4af14b99
Cesar Cavazos <me@ccavazos.co>
2017-09-15 22:57:07 -0700

    Update UI to look better

--- ImdbClient/NowPlayingViewController.swift
+++ ImdbClient/NowPlayingViewController.swift
@@ -12,15 +12,18 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
 
     @IBOutlet weak var moviesTableView: UITableView!
     
+    var movies: [NSDictionary] = [];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.moviesTableView.delegate = self
         self.moviesTableView.dataSource = self
 
-        self.moviesTableView.rowHeight = 140
+        self.moviesTableView.rowHeight = 162
         
         // Do any additional setup after loading the view.
+        fetchMovies()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,18 +31,52 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Network Request
+    func fetchMovies() {
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        
+        // Do any additional setup after loading the view.
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: .default,
+            delegate: nil,
+            delegateQueue: OperationQueue.main
+        )
+        
+        let task: URLSessionDataTask = session.dataTask(
+        with: request as URLRequest) { (data, response, error) in
+            if let data = data {
+                if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    print("responseDictionary: \(responseDictionary)")
+                    
+                    // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                    // This is how we get the 'response' field
+                    self.movies = responseDictionary["results"] as! [NSDictionary]
+                    // reload the table view
+                    self.moviesTableView.reloadData()
+                }
+            }
+        }
+        task.resume()
+        
+    }
+    
     // MARK: - TableView DataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = moviesTableView.dequeueReusableCell(withIdentifier: "MovieTableViewCell") as! MovieTableViewCell
         
-        cell.movieTitleLabel?.text = "Movie \(indexPath.row + 1)"
-        cell.overviewLabel?.text = "Overview \(indexPath.row + 1)"
-        cell.releaseDateLabel?.text = "Release Date \(Date())"
+        let movie = movies[indexPath.row]
+        
+        cell.movieTitleLabel?.text = movie["title"] as? String
+        cell.overviewLabel?.text = movie["overview"] as? String
+        let releaseDate = movie["release_date"] as! String
+        cell.releaseDateLabel?.text = "Release Date \(releaseDate)"
         
         return cell
     }
